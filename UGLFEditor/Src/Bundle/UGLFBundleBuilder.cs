1f97ad54eeaf7ba3679fc3f2c7bcb3a5a4db2154
fredshao <fredshao@aliyun.com>
2018-04-12 19:52:16 +0800

    Update Bundle Builder

--- UGLFEditor/Src/Bundle/UGLFBundleBuilder.cs
+++ UGLFEditor/Src/Bundle/UGLFBundleBuilder.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine;
 
 public class UGLFBundleBuilder
 {
+    public static void BuildBundle(BuildTarget _targetPlatform, string _abResPath)
+    {
 
+    }
 }
