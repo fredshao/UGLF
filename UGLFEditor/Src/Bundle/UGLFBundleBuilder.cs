6a4e709d59d6b2accb8f88a6e707981cee2715ee
fredshao <fredshao@aliyun.com>
2018-04-13 13:13:04 +0800

    Update Bundle Builder

--- UGLFEditor/Src/Bundle/UGLFBundleBuilder.cs
+++ UGLFEditor/Src/Bundle/UGLFBundleBuilder.cs
@@ -8,18 +8,36 @@ public class UGLFBundleBuilder
 {
     public static void BuildBundle(BuildTarget _targetPlatform, string _abResPath, string _bundleTargetPath, bool _autoDelManifestFile = true)
     {
+        string logStr = "";
         string directoryName = GetBundleSummaryFileName(_targetPlatform);
         string realBundleTargetPath = Path.Combine(_bundleTargetPath, directoryName);
         MakeDirectoryClean(realBundleTargetPath);
 
         List<AssetBundleBuild> builds = new List<AssetBundleBuild>();
-        foreach (string directory in Directory.GetDirectories(_abResPath))
+        foreach (string directory in Directory.GetDirectories(_abResPath, "*.*", SearchOption.AllDirectories))
         {
-            string[] files = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+            string bundleName = Path.GetFileNameWithoutExtension(directory);
+            string[] fileArray = Directory.GetFiles(directory, "*.*", SearchOption.TopDirectoryOnly);
+            List<string> fileList = new List<string>();
+            Debug.Log(bundleName.ToLower() + ".ab");
+            logStr += bundleName.ToLower() + ".ab\n";
+            foreach(string file in fileArray)
+            {
+                if(Path.GetExtension(file) == ".meta")
+                {
+                    continue;
+                }
+                fileList.Add(file);
+                Debug.Log("    -- " + file);
+                logStr += "    -- " + file + "\n";
+            }
+
             AssetBundleBuild build = new AssetBundleBuild();
-            build.assetBundleName = Path.GetFileNameWithoutExtension(directory) + ".ab";
-            build.assetNames = files;
+            build.assetBundleName = bundleName + ".ab";
+            build.assetNames = fileList.ToArray();
             builds.Add(build);
+
+            File.WriteAllText(Path.Combine(realBundleTargetPath, "log.txt"), logStr);
         }
 
         BuildAssetBundleOptions option = BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.ForceRebuildAssetBundle;
@@ -28,15 +46,16 @@ public class UGLFBundleBuilder
         if (_autoDelManifestFile)
         {
             string[] files = Directory.GetFiles(realBundleTargetPath);
-            foreach(string file in files)
+            foreach (string file in files)
             {
-                if(Path.GetExtension(file) == ".manifest")
+                if (Path.GetExtension(file) == ".manifest")
                 {
                     File.Delete(file);
                 }
             }
         }
     }
+
 
     private static void MakeDirectoryClean(string _platformDirectoy)
     {
