8d6e3a1223cbc1e6ac3090232a211a5940ca6dbc
fredshao <fredshao@aliyun.com>
2018-04-13 19:09:26 +0800

    Update Bundle Builder

--- UGLFEditor/Src/Bundle/UGLFBundleBuilder.cs
+++ UGLFEditor/Src/Bundle/UGLFBundleBuilder.cs
@@ -6,20 +6,43 @@ using System.IO;
 
 public class UGLFBundleBuilder
 {
-    public static void BuildBundle(BuildTarget _targetPlatform, string _abResPath, string _bundleTargetPath, bool _autoDelManifestFile = true)
+    /// <summary>
+    /// 打包所有的 Bundle
+    /// </summary>
+    /// <param name="_targetPlatform"></param>
+    /// <param name="_abResPath"></param>
+    /// <param name="_bundleTargetPath"></param>
+    /// <param name="_autoDelManifestFile"></param>
+    /// <returns></returns>
+    public static Dictionary<string, Hash128> BuildBundles(BuildTarget _targetPlatform, string _abResPath, string _bundleTargetPath, bool _autoDelManifestFile = true)
     {
+
+        if (!Directory.Exists(_abResPath))
+        {
+            Debug.LogError("请选择一个有效的目录!");
+            return null;
+        }
+
         string logStr = "";
         string directoryName = GetBundleSummaryFileName(_targetPlatform);
         string realBundleTargetPath = Path.Combine(_bundleTargetPath, directoryName);
         MakeDirectoryClean(realBundleTargetPath);
 
         List<AssetBundleBuild> builds = new List<AssetBundleBuild>();
-        foreach (string directory in Directory.GetDirectories(_abResPath, "*.*", SearchOption.AllDirectories))
+
+        string[] directories = Directory.GetDirectories(_abResPath, "*.*", SearchOption.AllDirectories);
+
+        if(directories == null || directories.Length == 0)
+        {
+            Debug.LogError("当前目录下没有子目录，请尝试使用 BuildSingleBundle!");
+            return null;
+        }
+
+        foreach (string directory in directories)
         {
             string bundleName = Path.GetFileNameWithoutExtension(directory);
             string[] fileArray = Directory.GetFiles(directory, "*.*", SearchOption.TopDirectoryOnly);
             List<string> fileList = new List<string>();
-            Debug.Log(bundleName.ToLower() + ".ab");
             logStr += bundleName.ToLower() + ".ab\n";
             foreach(string file in fileArray)
             {
@@ -28,35 +51,188 @@ public class UGLFBundleBuilder
                     continue;
                 }
                 fileList.Add(file);
-                Debug.Log("    -- " + file);
                 logStr += "    -- " + file + "\n";
             }
 
-            AssetBundleBuild build = new AssetBundleBuild();
-            build.assetBundleName = bundleName + ".ab";
-            build.assetNames = fileList.ToArray();
-            builds.Add(build);
+            if (fileList.Count > 0)
+            {
+                AssetBundleBuild build = new AssetBundleBuild();
+                build.assetBundleName = bundleName + ".ab";
+                build.assetNames = fileList.ToArray();
+                builds.Add(build);
+            }
 
             File.WriteAllText(Path.Combine(realBundleTargetPath, "log.txt"), logStr);
         }
 
+        if(builds.Count == 0)
+        {
+            Debug.LogError("未发现需要打包的资源");
+            return null;
+        }
+
         BuildAssetBundleOptions option = BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.ForceRebuildAssetBundle;
-        BuildPipeline.BuildAssetBundles(realBundleTargetPath, builds.ToArray(), option, _targetPlatform);
+        AssetBundleManifest manifest = BuildPipeline.BuildAssetBundles(realBundleTargetPath, builds.ToArray(), option, _targetPlatform);
 
         if (_autoDelManifestFile)
         {
-            string[] files = Directory.GetFiles(realBundleTargetPath);
-            foreach (string file in files)
-            {
-                if (Path.GetExtension(file) == ".manifest")
-                {
-                    File.Delete(file);
-                }
-            }
+            DeleteExtManifestFile(realBundleTargetPath);
         }
+
+        string manifestBundleFile = new DirectoryInfo(realBundleTargetPath).Name;
+        File.Delete(Path.Combine(realBundleTargetPath, manifestBundleFile));
+        return GetAllBundleInfoFromManifest(manifest);
+    }
+
+    /// <summary>
+    /// 打包一个目录及子目录的 Bundle
+    /// </summary>
+    /// <param name="_targetPlatform"></param>
+    /// <param name="_abResPath"></param>
+    /// <param name="_bundleTargetPath"></param>
+    /// <param name="_autoDelManifestFile"></param>
+    /// <returns></returns>
+    public static Dictionary<string, Hash128> BuildMultipleBundles(BuildTarget _targetPlatform, string _abResPath, string _bundleTargetPath, bool _autoDelManifestFile = true)
+    {
+
+        if (!Directory.Exists(_abResPath))
+        {
+            Debug.LogError("请选择一个有效的目录!");
+            return null;
+        }
+
+        string logStr = "";
+
+        MakeSureDirectoryExists(_bundleTargetPath);
+
+        List<AssetBundleBuild> builds = new List<AssetBundleBuild>();
+
+        string[] directories = Directory.GetDirectories(_abResPath, "*.*", SearchOption.AllDirectories);
+
+        if (directories == null || directories.Length == 0)
+        {
+            Debug.LogError("当前目录下没有子目录，请尝试使用 BuildSingleBundle!");
+            return null;
+        }
+
+        foreach (string directory in directories)
+        {
+            string bundleName = Path.GetFileNameWithoutExtension(directory);
+            string[] fileArray = Directory.GetFiles(directory, "*.*", SearchOption.TopDirectoryOnly);
+            List<string> fileList = new List<string>();
+            logStr += bundleName.ToLower() + ".ab\n";
+            foreach (string file in fileArray)
+            {
+                if (Path.GetExtension(file) == ".meta")
+                {
+                    continue;
+                }
+                fileList.Add(file);
+                logStr += "    -- " + file + "\n";
+            }
+
+            if (fileList.Count > 0)
+            {
+                AssetBundleBuild build = new AssetBundleBuild();
+                build.assetBundleName = bundleName + ".ab";
+                build.assetNames = fileList.ToArray();
+                builds.Add(build);
+            }
+
+            File.WriteAllText(Path.Combine(_bundleTargetPath, "log.txt"), logStr);
+        }
+
+        if (builds.Count == 0)
+        {
+            Debug.LogError("未发现需要打包的资源");
+            return null;
+        }
+
+        BuildAssetBundleOptions option = BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.ForceRebuildAssetBundle;
+        AssetBundleManifest manifest = BuildPipeline.BuildAssetBundles(_bundleTargetPath, builds.ToArray(), option, _targetPlatform);
+
+        if (_autoDelManifestFile)
+        {
+            DeleteExtManifestFile(_bundleTargetPath);
+        }
+
+        string manifestBundleFile = new DirectoryInfo(_bundleTargetPath).Name;
+        File.Delete(Path.Combine(_bundleTargetPath, manifestBundleFile));
+        return GetAllBundleInfoFromManifest(manifest);
+    }
+
+    /// <summary>
+    /// 打包单个目录的 Bundle
+    /// </summary>
+    /// <param name="_targetPlatform"></param>
+    /// <param name="_abSingleResPath"></param>
+    /// <param name="_bundleTargetPath"></param>
+    /// <param name="_autoDelManifestFile"></param>
+    /// <returns></returns>
+    public static Dictionary<string,Hash128> BuildSingleBundle(BuildTarget _targetPlatform, string _abSingleResPath, string _bundleTargetPath, bool _autoDelManifestFile = true)
+    {
+
+        if (!Directory.Exists(_abSingleResPath))
+        {
+            Debug.LogError("请选择一个有效的目录!");
+            return null;
+        }
+
+        string[] files = Directory.GetFiles(_abSingleResPath, "*.*", SearchOption.TopDirectoryOnly);
+
+        if(files == null || files.Length == 0)
+        {
+            Debug.LogError("未发现需要打包的资源");
+            return null;
+        }
+
+        string bundleName = Path.GetFileNameWithoutExtension(_abSingleResPath);
+        AssetBundleBuild build = new AssetBundleBuild();
+        build.assetBundleName = new DirectoryInfo(_abSingleResPath).Name + ".ab";
+        build.assetNames = GetAvaliableBundleAssetFile(_abSingleResPath);
+
+        AssetBundleBuild[] builds = { build };
+
+        BuildAssetBundleOptions option = BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.ForceRebuildAssetBundle;
+        AssetBundleManifest manifest = BuildPipeline.BuildAssetBundles(_bundleTargetPath, builds , option, _targetPlatform);
+
+        if (_autoDelManifestFile)
+        {
+            DeleteExtManifestFile(_bundleTargetPath);
+        }
+
+        string manifestBundleFile = new DirectoryInfo(_bundleTargetPath).Name;
+        File.Delete(Path.Combine(_bundleTargetPath, manifestBundleFile));
+        return GetAllBundleInfoFromManifest(manifest);
     }
 
 
+    /// <summary>
+    /// 获取一个目录下的所有非.meta文件，用于打包 Bundle
+    /// </summary>
+    /// <param name="_path"></param>
+    /// <returns></returns>
+    private static string[] GetAvaliableBundleAssetFile(string _path)
+    {
+        List<string> fileList = new List<string>();
+        string[] fileArray = Directory.GetFiles(_path, "*.*", SearchOption.TopDirectoryOnly);
+        foreach (string file in fileArray)
+        {
+            if (Path.GetExtension(file) == ".meta")
+            {
+                continue;
+            }
+            fileList.Add(file);
+        }
+
+        return fileList.ToArray();
+    }
+
+
+    /// <summary>
+    /// 把一个目录先删除再创建，确保干净
+    /// </summary>
+    /// <param name="_platformDirectoy"></param>
     private static void MakeDirectoryClean(string _platformDirectoy)
     {
         if (Directory.Exists(_platformDirectoy))
@@ -68,6 +244,60 @@ public class UGLFBundleBuilder
     }
 
 
+    /// <summary>
+    /// 如果一个目录不存在，则创建
+    /// </summary>
+    /// <param name="_path"></param>
+    private static void MakeSureDirectoryExists(string _path)
+    {
+        if(Directory.Exists(_path) == false)
+        {
+            Directory.CreateDirectory(_path);
+        }
+    }
+
+
+    /// <summary>
+    /// 删除扩展名为 manifest 的文件
+    /// </summary>
+    /// <param name="_path"></param>
+    private static void DeleteExtManifestFile(string _path)
+    {
+        foreach (string file in Directory.GetFiles(_path))
+        {
+            if (Path.GetExtension(file) == ".manifest")
+            {
+                File.Delete(file);
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// 从 Manifest 中获取所有的 Bundle 名字和 Bundle Hash128
+    /// </summary>
+    /// <param name="_manifest"></param>
+    /// <returns></returns>
+    private static Dictionary<string, Hash128> GetAllBundleInfoFromManifest(AssetBundleManifest _manifest)
+    {
+        Dictionary<string, Hash128> bundleInfoDict = new Dictionary<string, Hash128>();
+        string[] allBundleNames = _manifest.GetAllAssetBundles();
+        for (int i = 0; i < allBundleNames.Length; ++i)
+        {
+            string bundleName = allBundleNames[i];
+            Hash128 bundleHash = _manifest.GetAssetBundleHash(bundleName);
+            bundleInfoDict.Add(bundleName, bundleHash);
+        }
+
+        return bundleInfoDict;
+    }
+
+
+    /// <summary>
+    /// 获取对应平台Bundle应该放在的目录名
+    /// </summary>
+    /// <param name="_targetPlatform"></param>
+    /// <returns></returns>
     private static string GetBundleSummaryFileName(BuildTarget _targetPlatform)
     {
         switch (_targetPlatform)
