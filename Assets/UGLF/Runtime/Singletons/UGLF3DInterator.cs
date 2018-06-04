ba173b5560d78649291d0eafa7dd76234b17f1ac
fredshao <fredshao@aliyun.com>
2018-06-04 19:22:49 +0800

    添加3D物体点击触发器

--- Assets/UGLF/Runtime/Singletons/UGLF3DInterator.cs
+++ Assets/UGLF/Runtime/Singletons/UGLF3DInterator.cs
@@ -6,10 +6,6 @@ public delegate void UGLFHitObjectDelegate(GameObject _hitedObj);
 
 public class UGLF3DInterator : MonoBehaviour {
 
-    
-
-
-
     private event UGLFHitObjectDelegate onHitGameObject;
 
     public void AddListener(UGLFHitObjectDelegate _callback)
@@ -24,9 +20,39 @@ public class UGLF3DInterator : MonoBehaviour {
 
     private void Update()
     {
-        
+        if (onHitGameObject == null)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            if (UGLFUtil.IsPointerOverGameObject())
+            {
+                return;
+            }
+            else if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+            {
+                for (int i = 0; i < Input.touchCount; ++i)
+                {
+                    if (UGLFUtil.IsPointerOverGameObject())
+                    {
+                        return;
+                    }
+                }
+
+            }
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.collider != null)
+                {
+                    onHitGameObject(hit.collider.gameObject);
+                }
+            }
+        }
     }
-
-
 
 }
--- Assets/UGLF/Runtime/UGLF.cs
+++ Assets/UGLF/Runtime/UGLF.cs
@@ -3,6 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UGLF : MonoBehaviour {
+
+    private static UGLF _inst;
+    public static UGLF Inst {
+        get {
+            if(_inst == null)
+            {
+                GameObject obj = new GameObject("_UGLF");
+                _inst = obj.AddComponent<UGLF>();
+                DontDestroyOnLoad(obj);
+                _inst.Init();
+            }
+            return _inst;
+        }
+    }
+
+
+    private UGLF3DInterator _3dInterator = null;
+    public UGLF3DInterator interator3D {
+        get { return _3dInterator; }
+    }
+
+
+
+    private void Init()
+    {
+        _3dInterator = gameObject.AddComponent<UGLF3DInterator>();
+    }
+
+
+
     public static void AddUpdateCallback(Action _callback) {
 
     }
